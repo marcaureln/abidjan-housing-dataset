66f862fb713cd9312a1ab42e420917e8c3fa9907
Alex N'Guessan <alexmarcaureln@gmail.com>
2022-11-24 19:27:30 +0000

    Refactor

--- scraper/scraper/spiders/links_spider.py
+++ scraper/scraper/spiders/links_spider.py
@@ -3,7 +3,7 @@ import scrapy
 
 
 class LinksSpider(scrapy.Spider):
-    '''Spider to craw posts links from listing website.'''
+    '''Spider to crawl posts links from listing website.'''
     name = 'links'
     start_urls = [
         'https://deals.jumia.ci/abidjan/appartements-a-vendre',
@@ -11,16 +11,16 @@ class LinksSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
+        # link_selector is relative to the current post element (.post)
+        link_selector = 'div.text-area > div > div.announcement-infos > a::attr("href")'
+
         for post in response.css('.post'):
             yield {
-                'link': post.css(
-                    'div.text-area > div > div.announcement-infos > a::attr("href")'
-                ).get()
+                'link': post.css(link_selector).get()
             }
 
-        next_page = response.css(
-            '#tab1 > nav > ul > li.next > a::attr("href")'
-        ).get()
+        next_page_selector = '#tab1 > nav > ul > li.next > a::attr("href")'
+        next_page = response.css(next_page_selector).get()
 
         if next_page is not None:
             yield response.follow(next_page, self.parse)
