38780c3ab39c0444dde51e6db6224e5f7743036f
iAbdelRahim <110096698+iAbdelRahim@users.noreply.github.com>
2023-03-13 15:04:41 +0000

    poetry requirement, database pipeline, starting configuration

--- scraper/scraper/pipelines.py
+++ scraper/scraper/pipelines.py
@@ -6,8 +6,59 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import mysql.connector
+import tomli
 
 class ScraperPipeline:
+    def __init__(self):
+        with open("../scraper.toml", mode="r") as config_file:
+            self.config = tomli.load(config_file)
+            print(self.config)
+
+        self.conn = mysql.connector.connect(
+            host = self.config["database"]["host"],
+            user = self.config["database"]["user"],
+            password = self.config["database"]["password"],
+            database = self.config["database"]["name"]
+        )
+        
+        ## Create cursor, used to execute commands
+        self.cur = self.conn.cursor()
+        
+        ## Create houses table if none exists
+        self.cur.execute("""
+        CREATE TABLE IF NOT EXISTS houses(
+            house_id int NOT NULL auto_increment,
+            title VARCHAR(255),
+            description text,
+            price float,
+            tot_no_room int,
+            area text,
+            location text,
+            pub_date VARCHAR(255),
+            link text,
+            PRIMARY KEY (house_id)
+        )
+        """)
+    
     def process_item(self, item, spider):
-        return item
+        ## Define insert statement
+        self.cur.execute("""insert into houses (title, description, price, tot_no_room, area, location, pub_date, link) values (%s,%s,%s,%s,%s,%s,%s,%s)""", (
+            item["title"],
+            item["description"],
+            item["price"],
+            item["tot_no_room"],
+            item["area"],
+            item["location"],
+            item["pub_date"],
+            item["link"],
+        ))
+
+        ## Execute insert of data into database
+        self.conn.commit()
+
+    
+    def close_spider(self, spider):
+        ## Close cursor & connection to database 
+        self.cur.close()
+        self.conn.close()
--- scraper/scraper/spiders/links_spider.py
+++ scraper/scraper/spiders/links_spider.py
@@ -1,25 +1,32 @@
 '''Import scrapy - web scraping framework'''
+import os
 import scrapy
-
+import tomli
 
 class LinksSpider(scrapy.Spider):
     '''Spider to crawl posts links from listing website.'''
     name = 'links'
-    start_urls = [
-        'https://deals.jumia.ci/abidjan/appartements-a-vendre',
-        'https://deals.jumia.ci/abidjan/maisons-a-vendre'
-    ]
+
+    #TODO: Create a load config function to load config file
+    config_file = open(os.path.abspath("scraper/scraper.toml"), 'rb')
+    config = tomli.load(config_file)
+    print(config)        
+
+    start_urls = config["start_urls"]["links"]
 
     def parse(self, response):
+        with open("/scraper.toml", mode="r") as config_file:
+            config = tomli.load(config_file)
+
         # link_selector is relative to the current post element (.post)
-        link_selector = 'div.text-area > div > div.announcement-infos > a::attr("href")'
+        link_selector = config["selectors"]["links"]["link"]
 
         for post in response.css('.post'):
             yield {
                 'link': post.css(link_selector).get()
             }
 
-        next_page_selector = '#tab1 > nav > ul > li.next > a::attr("href")'
+        next_page_selector = config["selectors"]["links"]["next_page"]
         next_page = response.css(next_page_selector).get()
 
         if next_page is not None:
